20b894b0307c1607ef5cf89e761c8785f408cab1
aaknitt <andyknitt@gmail.com>
2020-10-17 21:54:22 -0500

    initial commit
    Signed-off-by: aaknitt <andyknitt@gmail.com>

---
+++ calc_points.py
@@ -0,0 +1,153 @@
+import json
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import csv
+from scipy.spatial.transform import Rotation as R
+
+#Three CSV data files containing the 2D coordinate points found from each of three camera positions
+#The three camera positions form an equilateral triangle around the pixel array
+#second camera position is to the right of the first position - counter-clockwise rotation
+#these files are generated from running pixel_automap.py three times - one from each camera location
+files = ['data/dataC1U1-5.csv','data/dataC2U1-5.csv','data/dataC3U1-5.csv']
+
+fov = [95, 53] #field of view of the camera [degreesHorizontal,degreesVertical]
+res = [1920, 1080] #resolution (in pixels) of the camera [Horizontal, Vertical]
+D = 370  #distance (in any units) from the camera to the center of the triangle
+
+data = []
+for i, file in enumerate(files):
+	with open(file, 'r') as f:
+		my_list = [list(map(float,rec)) for rec in csv.reader(f, delimiter=',')]
+		data.append(my_list)
+
+outpoints = [[[0,0,0,0] for i in range(len(data[0]))] for i in range(3)]
+
+#second camera is to the right of the first camera - counter-clockwise rotation
+combinations = [[data[0],data[1]],[data[1],data[2]],[data[2],data[0]]]
+for k, combodata in enumerate(combinations):
+	for i in range(0,len(combodata[0])):
+		if combodata[0][i][1] != 0 and combodata[1][i][1] != 0:  #camera 1 and camera 2 points are both valid
+			C1angle = 180 - (fov[0]/2.)*(combodata[0][i][1]-res[0]/2.)/(res[0]/2.)
+			C2angle = 120 - (fov[0]/2.)*(combodata[1][i][1]-res[0]/2.)/(res[0]/2.)
+			C1slope = math.tan(math.radians(C1angle))
+			C2slope = math.tan(math.radians(C2angle))
+			C1loc = [D,0]
+			C2loc = [-1*math.cos(math.radians(60))*D,math.sin(math.radians(60))*D]
+			C1intercept = C1loc[1]-C1slope*C1loc[0]  #y=mx+b -> b = y-mx
+			C2intercept = C2loc[1]-C2slope*C2loc[0]  #y=mx+b -> b = y-mx
+			x = (C2intercept-C1intercept)/(C1slope-C2slope)
+			y = C1slope*x+C1intercept
+			z1angle = -1*(fov[1]/2.)*(combodata[0][i][2]-res[1]/2.)/(res[1]/2.)
+			d1 = math.sqrt((x-C1loc[0])**2+(y-C1loc[1])**2)
+			z2angle = -1*(fov[1]/2.)*(combodata[1][i][2]-res[1]/2.)/(res[1]/2.)
+			d2 = math.sqrt((x-C2loc[0])**2+(y-C2loc[1])**2)
+			z1 = math.tan(math.radians(z1angle))*d1
+			z2 = math.tan(math.radians(z2angle))*d2
+			z = (z1+z2)/2.
+			outpoints[k][i] = [i,x,y,z]
+		else:
+			outpoints[k][i] = [i,0,0,0]
+#at this point, outpoints consists of three sets of 3D data points, each computed from a different combination
+#of two camera positions (positions 1&2, 2&3, and 3&1)
+			
+#remove index from the datasets and perform rotations around the Z-axis to align the data points
+data0 = np.array(outpoints[0])
+data0 = np.delete(data0,0,1)
+data1 = np.array(outpoints[1])
+data1 = np.delete(data1,0,1)
+r = R.from_euler('z',120,degrees=True)
+data1 = r.apply(data1)
+data2 = np.array(outpoints[2])
+data2 = np.delete(data2,0,1)
+r = R.from_euler('z',240,degrees=True)
+data2 = r.apply(data2)
+
+#turn zeros into NaN so that we don't use zero points (no pixel found) in the averaging below
+data0[data0==0] = np.nan
+data1[data1==0] = np.nan
+data2[data2==0] = np.nan
+
+#simple average of the X, Y, and Z coordinates found from each of the three different camera combinations
+#in many cases only two of the three camera locations will have found a pixel.
+out = []
+for i in range(len(data0)):
+	xavg = np.nanmean([data0[i][0],data1[i][0],data2[i][0]])
+	yavg = np.nanmean([data0[i][1],data1[i][1],data2[i][1]])
+	zavg = np.nanmean([data0[i][2],data1[i][2],data2[i][2]])
+	out.append([xavg,yavg,zavg])
+
+#single point linear interpolation of the output to fill in missing points
+'''
+for i, point in enumerate(out):
+	#if point[0] == 0 and i != 0 and i != len(out):
+	if point[0] == 0:
+		print("FOUND AN EMTPY SPOT")
+		print(i)
+		try:
+			if out[i-1][1] != 0 and out[i+1][1] != 0:
+				point[1] = (out[i-1][1] + out[i+1][1])/2.
+				point[2] = (out[i-1][2] + out[i+1][2])/2.
+				point[3] = (out[i-1][3] + out[i+1][3])/2.
+				print(point)
+		except:
+			print("FAIL")
+'''
+
+	
+#Create output file in xlights format
+#xlights format:
+#commas separate columns (width) (X), semicolons separate rows (height) (Y), "|" separate depth (Z)
+#pixel numbers (index of pixel) go between
+
+#determine the max size of the matrix to use for the model.  
+dataout = np.array(out)/3.   #divide numbers down to a reasonable resolution so the file is not too large
+dataout = np.nan_to_num(dataout,copy=False)
+dataout = (dataout.round()).astype('i')
+xmin = np.nanmin(dataout[:,0])
+xmax = np.nanmax(dataout[:,0])
+ymin = np.nanmin(dataout[:,1])
+ymax = np.nanmax(dataout[:,1])
+zmin = np.nanmin(dataout[:,2])
+zmax = np.nanmax(dataout[:,2])
+#start with a matrix filled with zeros.  
+xlout = np.zeros([xmax-xmin,zmax-zmin,ymax-ymin],'i')
+#for each pixel, populate the location in the matrix with the index number of the pixel
+#locations in the matrix without a pixel will remain zeros
+
+
+
+#global axis   xLights axis
+#    X             X
+#    Y             Z
+#    Z             Y
+for i, point in enumerate(dataout):
+	xlout[point[0]-xmin-1,-1*(point[2]-zmin-1),point[1]-ymin-1] = i+1
+
+#create an output string in xLights format
+outstring = ""
+for i, z in enumerate(xlout):
+	for j, y in enumerate(z):
+		for k,x in enumerate(y):
+			if x!= 0:
+				outstring = outstring + str(x)
+			if k != len(y)-1:
+				outstring = outstring + ","
+		if j != len(z)-1:
+			outstring = outstring + ";"
+	if i != len(xlout)-1:
+		outstring = outstring + "|"
+outxml = '<?xml version="1.0" encoding="UTF-8"?><custommodel name="Test" parm1="' + str(xmax-xmin) + '" parm2="' + str(zmax-zmin) + '" Depth="' + str(ymax-ymin) + '" StringType="RGB Nodes" Transparency="0" PixelSize="2" ModelBrightness="" Antialias="1" StrandNames="" NodeNames="" CustomModel="' + outstring + '" SourceVersion="2020.37"  ></custommodel>'
+f = open('test.xmodel','w')
+f.write(outxml)
+f.close()
+
+#plot our points
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(dataout[:,0], dataout[:,1], dataout[:,2])
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.show()
---
+++ pixel_automap.py
@@ -0,0 +1,313 @@
+from imutils import contours, grab_contours
+from skimage import metrics, measure
+import numpy as np
+import argparse
+import imutils
+import cv2
+import socket   
+import sys  
+import json
+import time
+from source import DMXSource
+import _thread
+import matplotlib.path as pltPath
+from playsound import playsound
+
+class Universe:
+	def __init__(self,source,channelcount):
+		self.source = source
+		self.pixelcount = int(channelcount/3)
+
+#Configure the Universes to send to when controlling the pixels
+U1 = Universe(DMXSource(universe=2000),510)
+U2 = Universe(DMXSource(universe=2001),510)
+U3 = Universe(DMXSource(universe=2002),510)
+U4 = Universe(DMXSource(universe=2003),510)
+U5 = Universe(DMXSource(universe=2004),60)
+universes = [U1,U2,U3,U4,U5]
+totalpixels = 750                 #total number of pixels to map
+onval = [100,100,100]             #RGB value to use when turning the pixels on for detection
+outfilename = 'out.csv'           #filename to put the output data - be sure to rename this for each camera position
+camera_resolution = [1920,1080]   #resolution (in pixels) of the camera [Horizontal, Vertical]
+camera_fov = [140,107]            #field of view of the camera [degreesHorizontal,degreesVertical]
+cap = cv2.VideoCapture('rtsp://username:password@cam.ip.add.ress:88/videoMain')  #Foscam X1 format address - others will be different
+
+class videosource():
+	def __init__(self, source, pixels):
+		self.source = source
+		self.currentFrame = None
+		self.polygon = None
+		self.point1 = [None,None]
+		self.point2 = [None,None]
+		self.zscale = 1
+		self.outputpoints = [[0,0,0]]*pixels
+		self.resolution = []
+		self.fov = []
+
+vsource = videosource(cap,totalpixels)
+vsource.resolution = camera_resolution
+vsource.fov = camera_fov
+
+CANVAS_SIZE = (600,800)
+FINAL_LINE_COLOR = (255, 255, 255)
+WORKING_LINE_COLOR = (127, 127, 127)
+
+#Function to continuoulsy get video data and update our videosource with the latest frame
+#this is needed to keep the frame buffer fresh with new data.  This function
+#will be running in its own thread that gets started below
+def updateFrame(videosource):
+	while(True):
+		ret, videosource.currentFrame = videosource.source.read()
+		cv2.waitKey(1)
+
+#Starts continuosly updating the images in a thread - if we don't do this, old images get stuck in the video buffer
+_thread.start_new_thread(updateFrame,(vsource,))
+
+time.sleep(1)
+
+class PolygonDrawer(object):
+	def __init__(self, window_name,videosource):
+		self.window_name = window_name # Name for our window
+		self.done = False # Flag signalling we're done
+		self.current = (0, 0) # Current position, so we can draw the line-in-progress
+		self.points = [] # List of points defining our polygon
+		self.videosource = videosource
+	def on_mouse(self, event, x, y, buttons, user_param):
+		# Mouse callback that gets called for every mouse event (i.e. moving, clicking, etc.)
+		if self.done: # Nothing more to do
+			return
+		if event == cv2.EVENT_MOUSEMOVE:
+			# We want to be able to draw the line-in-progress, so update current mouse position
+			self.current = (x, y)
+		elif event == cv2.EVENT_LBUTTONDOWN:
+			# Left click means adding a point at current position to the list of points
+			print("Adding point 1 #%d with position(%d,%d)" % (len(self.points), x, y))
+			self.points.append((x, y))
+		elif event == cv2.EVENT_RBUTTONDOWN:
+			# Right click means we're done
+			print("Adding point 1 #%d with position(%d,%d)" % (len(self.points), x, y))
+			self.done = True
+	def run(self):
+		# Let's create our working window and set a mouse callback to handle events
+		cv2.namedWindow(self.window_name, flags=cv2.WINDOW_NORMAL)
+		
+		while(not self.done):
+			# This is our drawing loop, we just continuously draw new images
+			# and show them in the named window
+			cv2.resizeWindow(self.window_name, 800, 600);
+			#cv2.imshow(self.window_name, np.zeros(CANVAS_SIZE, np.uint8))
+			cv2.imshow(self.window_name,self.videosource.currentFrame)
+			cv2.waitKey(1)
+			cv2.setMouseCallback(self.window_name, self.on_mouse)
+			height, width, channels = self.videosource.currentFrame.shape
+			CANVAS_SIZE = (height,width)
+			#canvas = np.zeros(CANVAS_SIZE, np.uint8)
+			if (len(self.points) > 0):
+				# Draw all the current polygon segments
+				cv2.polylines(self.videosource.currentFrame, np.array([self.points]), False, FINAL_LINE_COLOR, 1)
+				# And  also show what the current segment would look like
+				cv2.line(self.videosource.currentFrame, self.points[-1], self.current, WORKING_LINE_COLOR)
+			# Update the window
+			cv2.imshow(self.window_name, self.videosource.currentFrame)
+			# And wait 50ms before next iteration (this will pump window messages meanwhile)
+			if cv2.waitKey(50) == 27: # ESC hit
+				self.done = True
+
+		# User finised entering the polygon points, so let's make the final drawing
+		#canvas = np.zeros(CANVAS_SIZE, np.uint8)
+		# of a filled polygon
+		if (len(self.points) > 0):
+			cv2.fillPoly(self.videosource.currentFrame, np.array([self.points]), FINAL_LINE_COLOR)
+		# And show it
+		cv2.imshow(self.window_name, self.videosource.currentFrame)
+		# Waiting for the user to press any key
+		cv2.waitKey(1000)
+		self.videosource.polygon = self.points
+		cv2.destroyWindow(self.window_name)
+		return self.videosource.currentFrame
+
+output = []
+def all_off():
+	#shut all pixels off
+	for universe in universes:
+		universe.source.send_data(data=[0,0,0]*universe.pixelcount)
+def everyother():
+	#turn every other pixel on - for testing
+	for universe in universes:
+		universe.source.send_data(data=onval*universe.pixelcount)
+		counter = 0
+		for i,element in enumerate(data):
+			if counter == 3 or counter == 4 or counter == 5:
+				data[i] = 0
+			counter = counter + 1
+			if counter > 5:
+				counter = 0
+def all_on():
+	#turn all pixels on white
+	for universe in universes:
+		universe.source.send_data(data=onval*universe.pixelcount)
+
+#display alignment overlays until the user hits ESK key
+done = False
+while done == False:
+	image = vsource.currentFrame
+	cv2.namedWindow(str(i), flags=cv2.WINDOW_NORMAL)
+	image = cv2.line(image,(round(vsource.resolution[0]/2),0),(round(vsource.resolution[0]/2),vsource.resolution[1]),(255,0,0),5)
+	image = cv2.line(image,(0,round(vsource.resolution[1]/2)),(vsource.resolution[0],round(vsource.resolution[1]/2)),(255,0,0),5)
+	pixel30neg = round(vsource.resolution[0]/2 - vsource.resolution[0]/vsource.fov[0]*30)
+	pixel30pos = round(vsource.resolution[0]/2 + vsource.resolution[0]/vsource.fov[0]*30)
+	image = cv2.line(image,(pixel30neg,0),(pixel30neg,vsource.resolution[1]),(0,255,0),5)
+	image = cv2.line(image,(pixel30pos,0),(pixel30pos,vsource.resolution[1]),(0,255,0),5)
+	cv2.imshow(str(i),image)
+	cv2.resizeWindow(str(i), 800, 450);
+	if cv2.waitKey(50) == 27: # ESC hit
+		done = True
+		
+cv2.destroyWindow(str(i))
+
+#Polygon masking - turn on all pixels and allow the user to draw a polygon around them to prevent
+#detecting light sources outside the area of interest
+all_on()  #turn all pixels on
+time.sleep(1)
+pd = PolygonDrawer("Polygon1",vsource)
+image = pd.run()
+print("Polygon = %s" % vsource.polygon)
+
+#function to get user input during pixel mapping
+#if the image processing can't find a pixel, the user can click on its location to specify the coordinates
+#alternatively, the user can click outside the polygon area of interest and the coordinates will be set to 0,0 (no pixel found)
+def on_mouse( event, x, y, buttons, mystuff):
+	index = mystuff[0]
+	videosource = mystuff[1]
+	if event == cv2.EVENT_LBUTTONDOWN:
+		# Left click means adding a point at current position to the list of points
+		path = pltPath.Path(videosource.polygon)
+		if path.contains_point([x,y]):
+			videosource.outputpoints[index] = [index,x,y]
+			print("Adding point #%d with position(%d,%d)" % (index, x, y))
+		else:
+			videosource.outputpoints[index] = [index,0,0]
+			print("Adding point #%d with position(%d,%d)" % (index,0,0))
+
+all_off()
+time.sleep(1)
+cv2.namedWindow("Camera1", flags=cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Camera1", 800, 600);
+for unum, universe in enumerate(universes):
+	for index in range(0,universe.pixelcount):
+		attempts = 1
+		while attempts >0:
+			pixelout = {}
+			all_off()
+			time.sleep(0.7)
+			image_off = vsource.currentFrame
+			print("image_off")
+			cv2.imshow("Camera1",image_off)
+			cv2.resizeWindow("Camera1", 800, 600);
+			cv2.waitKey(500)
+			universe.source.send_data(data=[0,0,0]*(index) + onval + [0,0,0]*(universe.pixelcount-index-1))
+			time.sleep(0.7)
+			image = vsource.currentFrame
+			print("image")
+			cv2.imshow("Camera1",image)
+			cv2.waitKey(500)
+			####MASK OUT THE PORTIONS OF THE IMAGES WE DON'T CARE ABOUT###########
+			height, width, channels = image_off.shape
+			canvas = np.zeros((height,width), np.uint8)
+			polymask = cv2.fillPoly(canvas, np.array([vsource.polygon]), [255,255,255])
+			masked_image_off = cv2.bitwise_and(image_off,image_off, mask = polymask)
+			masked_image = cv2.bitwise_and(image,image, mask = polymask)
+			#######################IMAGE DIFFERENCE###############################
+			#https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+			gray = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
+			gray_off = cv2.cvtColor(masked_image_off, cv2.COLOR_BGR2GRAY)
+			# compute the Structural Similarity Index (SSIM) between the two
+			# images, ensuring that the difference image is returned
+			#(score, diff) = measure.compare_ssim(gray, gray_off, full=True)
+			(score, diff) = metrics.structural_similarity(gray, gray_off, full=True)
+			diff = (diff * 255).astype("uint8")
+			print("SSIM: {}".format(score))
+			# threshold the difference image, followed by finding contours to
+			# obtain the regions of the two input images that differ
+			thresh = cv2.threshold(diff, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+			cnts = grab_contours(cnts)
+			diff_mask = thresh
+			diff_masked_gray = cv2.bitwise_and(gray, gray, mask = diff_mask)
+			blurred = cv2.GaussianBlur(diff_masked_gray, (11, 11), 0)
+			thresh2 = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)[1]
+			# perform a series of erosions and dilations to remove
+			# any small blobs of noise from the thresholded image
+			#thresh = cv2.erode(thresh, None, iterations=2)
+			#thresh = cv2.dilate(thresh, None, iterations=4)
+			# perform a connected component analysis on the thresholded
+			# image, then initialize a mask to store only the "large"
+			# components
+			labels = measure.label(thresh2, connectivity=2, background=0)
+			mask = np.zeros(thresh2.shape, dtype="uint8")
+			# loop over the unique components
+			for label in np.unique(labels):
+				# if this is the background label, ignore it
+				if label == 0:
+					continue
+				# otherwise, construct the label mask and count the
+				# number of pixels 
+				labelMask = np.zeros(thresh2.shape, dtype="uint8")
+				labelMask[labels == label] = 255
+				numPixels = cv2.countNonZero(labelMask)
+				# if the number of pixels in the component is sufficiently
+				# large, then add it to our mask of "large blobs"
+				if numPixels > 50:
+					mask = cv2.add(mask, labelMask)
+			# find the contours in the mask, then sort them from left to
+			# right
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+				cv2.CHAIN_APPROX_SIMPLE)
+			print(cnts)
+			cnts = cnts[0] if (imutils.is_cv2() or imutils.is_cv4()) else cnts[1]
+			if len(cnts) > 0:
+				cnts = contours.sort_contours(cnts)[0]
+				# loop over the contours
+				for (i, c) in enumerate(cnts):
+					# draw the bright spot on the image
+					(x, y, w, h) = cv2.boundingRect(c)
+					((cX, cY), radius) = cv2.minEnclosingCircle(c)
+					cv2.circle(image, (int(cX), int(cY)), int(radius),
+						(0, 0, 255), 3)
+					cv2.putText(image, str(cX) + " " + str(cY), (x, y - 15),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+					# show the output image
+					cv2.imshow("Camera1", image)
+					cv2.waitKey(1)
+				if len(cnts)== 1:
+					attempts = 0
+					print("Pixel " + str(index) + " coordinates: [" + str(x) + "," + str(y) + "]")
+					vsource.outputpoints[unum*170+index] = [unum*170+index,cX,cY]
+				else:
+					print("too many bright spots, trying again!")
+					attempts = attempts + 1
+					cv2.imshow("Camera1",image)
+					cv2.waitKey(5)
+			else:
+				print("No bright spots found")
+				attempts = attempts + 1
+			if attempts >= 2:
+				print('too many points attempts - click on the pixel to locate or click outside of polygon to skip') 
+				playsound('alert.wav')
+				cv2.imshow("Camera1", image)
+				cv2.setMouseCallback("Camera1",on_mouse,[unum*170+index,vsource])
+				done = 0
+				while done == 0:
+					cv2.waitKey(50)
+					if vsource.outputpoints[unum*170+index] != [0,0,0]:
+						done = 1
+					else:
+						done = 0
+				attempts = 0
+		with open('data1.txt', 'a') as outfile:
+			outfile.write(str(unum*170+index) + ',' + str(vsource.outputpoints[unum*170+index][1]) + ',' + str(vsource.outputpoints[unum*170+index][2]) + "\n")
+all_off()
+
+with open(outfilename, 'w') as outfile:
+	#json.dump([sorted_by_pixel_x, sorted_by_pixel_y], outfile)
+	json.dump(videosource.outputpoints, outfile)
